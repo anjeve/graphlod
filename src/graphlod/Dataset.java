ff97a6996275c67b0edb95682351da74c90986bc
Christian Dullweber <xchrdw@gmail.com>
2014-11-05 15:23:03 +0100

    remove manual iterators, cleanup deep nested ifs

--- src/graphlod/Dataset.java
+++ src/graphlod/Dataset.java
@@ -5,7 +5,6 @@ import java.io.FileNotFoundException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.Iterator;
 
 import org.jgraph.graph.DefaultEdge;
 import org.jgrapht.DirectedGraph;
@@ -26,74 +25,67 @@ public class Dataset {
 		NxParser nxp;
 		try {
 			nxp = new NxParser(new FileInputStream(dataset)/*, false*/);
-			
-			ArrayList<String> removeVertices = new ArrayList<String>();
-			
-			while (nxp.hasNext()) {
-			  Node[] ns = nxp.next();
-			  if (ns.length == 3) {
-				  
-				  try {
-					  /*
-					  for (Node n: ns) 
-					    {
-					      System.out.print(n.toN3());
-					      System.out.print(" ");
-					    }
-					    System.out.println(".");
-					    */
-					  String objectUri = (new URL(ns[2].toString())).toString();
-					  String subjectUri = (new URL(ns[0].toString())).toString();
-					  String propertyUri = (new URL(ns[1].toString())).toString();
-					  
-					  if (!subjectUri.equals(objectUri)) {
-						  if (propertyUri.equals("http://www.w3.org/1999/02/22-rdf-syntax-ns#type")) {
-							  if (objectUri.equals("http://www.w3.org/1999/02/22-rdf-syntax-ns#Property")) {
-								  removeVertices.add(subjectUri);
-								  removeVertices.add(objectUri);
-							  } else if (objectUri.equals("http://www.w3.org/1999/02/22-rdf-syntax-ns#Class")) {
-								  removeVertices.add(subjectUri);
-								  removeVertices.add(objectUri);
-							  }
-						  } else {
-							  // TODO exclude schema (properties, classes)
-							  // list of subject uris that have one of these properties:
-							  // remove them also afterwards
-							
-							  if (!g.containsVertex(subjectUri)) {
-								  g.addVertex(subjectUri);
-								  nrVertex += 1;
-							  }
-							  if (!g.containsVertex(objectUri)) {
-								  g.addVertex(objectUri);
-								  nrVertex += 1;
-							  }
-							  if (g instanceof DirectedGraph) {
-								  g.addEdge(subjectUri, objectUri);
-								  nrEdges += 1;
-							  } else {
-								  if (!g.containsEdge(subjectUri, objectUri) && !g.containsEdge(objectUri, subjectUri)) {
-									  g.addEdge(subjectUri, objectUri);
-									  nrEdges += 1;
-								  }
-							  }
-						  }
-					  }
-				  
-				  } catch (MalformedURLException e) {
-						//e.printStackTrace();
-					}
-			  }
+		} catch (FileNotFoundException e) {
+			throw new RuntimeException(e);
+		}
+
+		ArrayList<String> removeVertices = new ArrayList<String>();
+
+		while (nxp.hasNext()) {
+			Node[] nodes = nxp.next();
+			if (nodes.length != 3) {
+				continue;
 			}
-			
-			for (Iterator<String> iterator = removeVertices.iterator(); iterator.hasNext();) {
-				String vertex = (String) iterator.next();
-				if (g.containsVertex(vertex)) {
-					g.removeVertex(vertex);
+			String subjectUri;
+			String propertyUri;
+			String objectUri;
+			try {
+				objectUri = (new URL(nodes[2].toString())).toString();
+				subjectUri = (new URL(nodes[0].toString())).toString();
+				propertyUri = (new URL(nodes[1].toString())).toString();
+			} catch (MalformedURLException e) {
+				continue;
+			}
+			if (subjectUri.equals(objectUri)) {
+				continue; // TODO: why that?
+			}
+
+			if (propertyUri.equals("http://www.w3.org/1999/02/22-rdf-syntax-ns#type")) {
+				if (objectUri.equals("http://www.w3.org/1999/02/22-rdf-syntax-ns#Property")) {
+					removeVertices.add(subjectUri);
+					removeVertices.add(objectUri);
+				} else if (objectUri.equals("http://www.w3.org/1999/02/22-rdf-syntax-ns#Class")) {
+					removeVertices.add(subjectUri);
+					removeVertices.add(objectUri);
+				}
+			} else {
+				// TODO exclude schema (properties, classes)
+				// list of subject uris that have one of these properties:
+				// remove them also afterwards
+
+				if (!g.containsVertex(subjectUri)) {
+					g.addVertex(subjectUri);
+					nrVertex += 1;
+				}
+				if (!g.containsVertex(objectUri)) {
+					g.addVertex(objectUri);
+					nrVertex += 1;
+				}
+				if (g instanceof DirectedGraph) {
+					g.addEdge(subjectUri, objectUri);
+					nrEdges += 1;
+				} else {
+					if (!g.containsEdge(subjectUri, objectUri) && !g.containsEdge(objectUri, subjectUri)) {
+						g.addEdge(subjectUri, objectUri);
+						nrEdges += 1;
+					}
 				}
 			}
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
+		}
+		for (String vertex : removeVertices) {
+			if (g.containsVertex(vertex)) {
+				g.removeVertex(vertex);
+			}
 		}
 	}
 
--- src/graphlod/GraphLOD.java
+++ src/graphlod/GraphLOD.java
@@ -4,34 +4,33 @@ import java.text.NumberFormat;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Locale;
 import java.util.Set;
 
 import org.apache.log4j.Logger;
 
-public class GraphLOD {
-	// private UndirectedGraph<String, DefaultEdge> g = new SimpleGraph<String, DefaultEdge>(DefaultEdge.class);
-	private String datasetLocation = "/Users/anjeve/Desktop/keket backup/mappingbased_properties_en.nt";
-	private static final Logger logger = Logger.getLogger(GraphLOD.class);
-	
-	public GraphLOD(String[] args) {
-		
-		long t1 = System.currentTimeMillis();
-		for (int i = 0; i < args.length; i++) {
-			datasetLocation = args[i];
-		}
+import net.sourceforge.argparse4j.ArgumentParsers;
+import net.sourceforge.argparse4j.impl.Arguments;
+import net.sourceforge.argparse4j.inf.ArgumentParser;
+import net.sourceforge.argparse4j.inf.ArgumentParserException;
+import net.sourceforge.argparse4j.inf.Namespace;
 
+public class GraphLOD {
+	private static String defaultDatasetLocation = "/Users/anjeve/Desktop/keket backup/mappingbased_properties_en.nt";
+	private static final Logger logger = Logger.getLogger(GraphLOD.class);
+
+	public GraphLOD(String datasetLocation, boolean skipChromaticNumber) {
+		long t1 = System.currentTimeMillis();
 		Dataset dataset = new Dataset(datasetLocation);
 		//DirectedGraph<String, DefaultEdge> graph = dataset.getGraph();
 		GraphFeatures graphFeatures = new GraphFeatures(dataset);
-		
+
 		long t2 = System.currentTimeMillis();
 		System.out.println("Loading the dataset took " + (t2 - t1) + " milliseconds to execute.");
-				
-		System.out.println("Vertices: "+ formatInt(dataset.getVertices()));
-		System.out.println("Edges: "+ formatInt(dataset.getEdges()));
+
+		System.out.println("Vertices: " + formatInt(dataset.getVertices()));
+		System.out.println("Edges: " + formatInt(dataset.getEdges()));
 
 		long t5 = System.currentTimeMillis();
 		if (graphFeatures.isConnected()) {
@@ -39,51 +38,45 @@ public class GraphLOD {
 		} else {
 			System.out.println("Connectivity: no");
 		}
-		
+
 		List<Set<String>> sets = graphFeatures.getConnectedSets();
 		System.out.println("Connected sets: " + formatInt(sets.size()));
 
 		ArrayList<Integer> componentSizes = new ArrayList<Integer>();
-		for (Iterator<Set<String>> iterator = sets.iterator(); iterator.hasNext();) {
-			Set<String> component = iterator.next();
+		for (Set<String> component : sets) {
 			componentSizes.add(component.size());
 		}
 		Collections.sort(componentSizes);
-		
+
 		Set<Integer> uniqComponentSizes = new HashSet<Integer>(componentSizes);
-		System.out.println("  Components (and sizes): "); 
-		for (Iterator<Integer> iterator = uniqComponentSizes.iterator(); iterator
-				.hasNext();) {
-			Integer integer = (Integer) iterator.next();
+		System.out.println("  Components (and sizes): ");
+		for (Integer integer : uniqComponentSizes) {
 			int freq = Collections.frequency(componentSizes, integer);
 			System.out.println("    " + freq + " x " + integer);
 		}
-		
+
 //		System.out.println(" Set sizes: " + StringUtils.join(componentSizes,","));
-		
+
 		List<Set<String>> sci_sets = graphFeatures.getStronglyConnectedSets();
 		System.out.println("Strongly connected components: " + formatInt(sci_sets.size()));
-		
+
 		ArrayList<Integer> sciSizes = new ArrayList<Integer>();
-		for (Iterator<Set<String>> iterator = sci_sets.iterator(); iterator.hasNext();) {
-			Set<String> component = iterator.next();
+		for (Set<String> component : sci_sets) {
 			sciSizes.add(component.size());
 		}
 		Collections.sort(componentSizes);
 
 		Set<Integer> uniqSCSizes = new HashSet<Integer>(sciSizes);
-		System.out.println("  Components (and sizes): "); 
-		for (Iterator<Integer> iterator = uniqSCSizes.iterator(); iterator
-				.hasNext();) {
-			Integer integer = (Integer) iterator.next();
+		System.out.println("  Components (and sizes): ");
+		for (Integer integer : uniqSCSizes) {
 			int freq = Collections.frequency(sciSizes, integer);
 			System.out.println("    " + freq + " x " + integer);
 		}
 
-	
+
 		long t6 = System.currentTimeMillis();
 		System.out.println("Getting the connectivity took " + (t6 - t5) + " milliseconds to execute.");
-		
+
 		if (graphFeatures.isConnected()) {
 			long t3 = System.currentTimeMillis();
 			double diameter = graphFeatures.getDiameter();
@@ -97,37 +90,44 @@ public class GraphLOD {
 
 		ArrayList<Integer> edgeCounts = graphFeatures.getEdgeCounts();
 		System.out.println("Average links: " + calculateAverage(edgeCounts));
-		
-		
-		long t7 = System.currentTimeMillis();
-		int cN = graphFeatures.getChromaticNumber();
-		System.out.println("Chromatic Number: " + cN);
-		long t8 = System.currentTimeMillis();
-		System.out.println("Getting the Chromatic Number took " + (t8 - t7) + " milliseconds to execute.");
-	       
+
+		if (!skipChromaticNumber) {
+			long t7 = System.currentTimeMillis();
+			int cN = graphFeatures.getChromaticNumber();
+			System.out.println("Chromatic Number: " + cN);
+			long t8 = System.currentTimeMillis();
+			System.out.println("Getting the Chromatic Number took " + (t8 - t7) + " milliseconds to execute.");
+		}
 	}
 
-	private double calculateAverage(List <Integer> marks) {
-	  Integer sum = 0;
-	  if(!marks.isEmpty()) {
-	    for (Integer mark : marks) {
-	        sum += mark;
-	    }
-	    return sum.doubleValue() / marks.size();
-	  }
-	  return sum;
+	private double calculateAverage(List<Integer> marks) {
+		Integer sum = 0;
+		if (!marks.isEmpty()) {
+			for (Integer mark : marks) {
+				sum += mark;
+			}
+			return sum.doubleValue() / marks.size();
+		}
+		return sum;
 	}
-	
+
 	private String formatInt(int integer) {
 		return NumberFormat.getNumberInstance(Locale.US).format(integer);
 	}
 
-	public static void main (final String[] args) {
-		if (args.length > 1) {
-			logger.error("Too many paramaters. Please provide one dataset location only.");
-			throw new IllegalArgumentException();
+	public static void main(final String[] args) {
+		ArgumentParser parser = ArgumentParsers.newArgumentParser("GraphLOD")
+				.defaultHelp(true).description("calculates graph features.");
+		parser.addArgument("database").nargs("?").setDefault(defaultDatasetLocation);
+		parser.addArgument("--skipChromatic").action(Arguments.storeTrue());
+		Namespace result = null;
+		try {
+			result = parser.parseArgs(args);
+		} catch (ArgumentParserException e) {
+			parser.handleError(e);
+			System.exit(1);
 		}
-		new GraphLOD(args);
+		new GraphLOD(result.getString("database"), result.getBoolean("skipChromatic"));
 	}
 
 }
