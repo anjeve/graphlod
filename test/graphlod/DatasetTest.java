8bf3a82956078df21535261dc7cc000fbde46e37
Christian Dullweber <xchrdw@gmail.com>
2014-11-12 14:46:58 +0100

    add missing test dependencies

--- test/graphlod/DatasetTest.java
+++ test/graphlod/DatasetTest.java
@@ -1,44 +1,45 @@
 package graphlod;
 
 
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
+
 import java.util.ArrayList;
 
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.hamcrest.Matchers.equalTo;
-import static org.junit.Assert.assertThat;
-
 public class DatasetTest {
-	ArrayList<String> lines;
 
-	@Before
-	public void setUp() {
-		lines = new ArrayList<>();
-	}
+    ArrayList<String> lines;
 
-	@Test
-	public void testGraphParse() {
-		lines.add(createStatement("a","p1", "b"));
-		lines.add(createStatement("a","p2", "c"));
-		Dataset dataset = new Dataset(lines);
+    @Before
+    public void setUp() {
+        lines = new ArrayList<>();
+    }
 
-		assertThat(dataset.getVertices(), equalTo(3));
-		assertThat(dataset.getEdges(), equalTo(2));
-	}
+    @Test
+    public void testGraphParse() {
+        lines.add(createStatement("a", "p1", "b"));
+        lines.add(createStatement("a", "p2", "c"));
+        Dataset dataset = new Dataset(lines);
 
-	@Test
-	public void literalsDontCount() {
-		lines.add(createStatement("a","p1", "b"));
-		lines.add("<http://%s> <http://%s> \"literal\" .");
-		Dataset dataset = new Dataset(lines);
+        assertThat(dataset.getVertices(), equalTo(3));
+        assertThat(dataset.getEdges(), equalTo(2));
+    }
 
-		assertThat(dataset.getVertices(), equalTo(2));
-		assertThat(dataset.getEdges(), equalTo(1));
-	}
+    @Test
+    public void literalsDontCount() {
+        lines.add(createStatement("a", "p1", "b"));
+        lines.add("<http://%s> <http://%s> \"literal\" .");
+        Dataset dataset = new Dataset(lines);
+
+        assertThat(dataset.getVertices(), equalTo(2));
+        assertThat(dataset.getEdges(), equalTo(1));
+    }
 
 
-	private String createStatement(String subject, String predicate, String object) {
-		return String.format("<http://%s> <http://%s> <http://%s> .", subject, predicate, object);
-	}
+    private String createStatement(String subject, String predicate, String object) {
+        return String.format("<http://%s> <http://%s> <http://%s> .", subject, predicate, object);
+    }
 }
