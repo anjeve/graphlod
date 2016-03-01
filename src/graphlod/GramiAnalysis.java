3590292937ca8e879436fed6ab575f52e65fda57
Christian Dullweber <xchrdw@gmail.com>
2016-03-01 11:52:48 +0100

    improve grami

--- src/graphlod/GramiAnalysis.java
+++ src/graphlod/GramiAnalysis.java
@@ -16,13 +16,18 @@ import com.google.common.collect.Maps;
 import com.grami.directed_patterns.dataStructures.Graph;
 import com.grami.directed_patterns.dataStructures.HPListGraph;
 import com.grami.directed_patterns.search.Searcher;
+import com.grami.directed_patterns.utilities.CommandLineParser;
 
 public class GramiAnalysis {
 
     int shortestDistance = 1;
-    int frequencyThreshold = 1000;
+    int frequencyThreshold = 5;
+
 
     public List<GraphPattern> run(Dataset dataset) {
+        //parse the command line arguments
+        CommandLineParser.parse(new String[]{"mlabels=false", "approximate=0.0", "approxConst=0"});
+
         List<Graph.Node> nodes = Lists.newArrayList();
         BiMap<String, Integer> nodeMap = HashBiMap.create();
         Map<String, Integer> classMap = createClassMap(dataset);
@@ -30,13 +35,14 @@ public class GramiAnalysis {
         for (String vertex : dataset.getGraph().vertexSet()) {
             int label = classMap.get(dataset.getClassForSubject(vertex));
             nodes.add(new Graph.Node(i, label));
-            nodeMap.put(vertex, i++);
+            nodeMap.put(vertex, i);
+            i++;
         }
         List<Graph.Edge> edges = createEdges(dataset, nodeMap);
 
         Graph graph = new Graph(1, frequencyThreshold);
 
-        Searcher<String, String> sr;
+        Searcher<Integer, Integer> sr;
         try {
             graph.loadFromData(nodes, edges);
             sr = new Searcher<>(graph, frequencyThreshold, shortestDistance);
@@ -48,20 +54,19 @@ public class GramiAnalysis {
         sr.search();
 
         List<GraphPattern> pattern = Lists.newArrayList();
-        for (HPListGraph<String, String> srGraph : sr.result) {
-            pattern.add(getGraphPattern(srGraph, nodeMap.inverse()));
+        for (HPListGraph<Integer, Integer> srGraph : sr.result) {
+            pattern.add(getGraphPattern(srGraph, nodeMap.inverse(), dataset));
         }
 
         return pattern;
     }
 
     private Map<String, Integer> createClassMap(Dataset dataset) {
-        Map<String, Integer> classMap;
-        classMap = Maps.newHashMap();
+        Map<String, Integer> classMap = Maps.newHashMap();
         classMap.put("null", 0);
         int i = 1;
         for (String s : dataset.getOntologyClasses()) {
-            classMap.put(s, i);
+            classMap.put(s, i++);
         }
         return classMap;
     }
@@ -78,15 +83,18 @@ public class GramiAnalysis {
         return edges;
     }
 
-    private GraphPattern getGraphPattern(HPListGraph<String, String> graph, Map<Integer, String> nodeMap) {
+    private GraphPattern getGraphPattern(HPListGraph<Integer, Integer> graph, Map<Integer, String> nodeMap, Dataset dataset) {
         List<String> pNodes = Lists.newArrayList();
         Map<String, String> pEdges = Maps.newHashMap();
 
         BitSet nodes = graph.getNodes();
         BitSet edges = graph.getEdges();
+        int i = 1 + 1;
         for (int nodeIdx = nodes.nextSetBit(0); nodeIdx >= 0; nodeIdx = nodes.nextSetBit(nodeIdx + 1)) {
-            String label = nodeMap.get(nodeIdx);
-            pNodes.add(label);
+            String node = nodeMap.get(nodeIdx);
+            String label = dataset.getClassForSubject(node);
+            node += "(" + label + ")";
+            pNodes.add(node);
         }
 
         for (int edgeIdx = edges.nextSetBit(0); edgeIdx >= 0; edgeIdx = edges.nextSetBit(edgeIdx + 1)) {
@@ -99,23 +107,26 @@ public class GramiAnalysis {
                 pEdges.put(node2, node1);
             }
         }
-        return new GraphPattern(pNodes, pEdges);
+        return new GraphPattern(pNodes, pEdges, graph.toString());
     }
 
     public static class GraphPattern {
         public List<String> nodes;
         public Map<String, String> edges;
+        public String graph;
 
-        public GraphPattern(List<String> nodes, Map<String, String> edges) {
+        public GraphPattern(List<String> nodes, Map<String, String> edges, String graph) {
             this.nodes = nodes;
             this.edges = edges;
+            this.graph = graph;
         }
 
         @Override
         public String toString() {
             return MoreObjects.toStringHelper(this)
                     .add("nodes", nodes)
-                    .add("edges", edges)
+                    .add("\nedges", edges)
+                    .add("\ngraph", "\n"+graph)
                     .toString();
         }
     }
--- src/graphlod/GspanAnalysis.java
+++ src/graphlod/GspanAnalysis.java
@@ -1,4 +1,19 @@
 package graphlod;
 
+import de.parsemis.Miner;
+import de.parsemis.graph.Graph;
+import de.parsemis.miner.environment.Settings;
+import de.parsemis.miner.general.Fragment;
+import graphlod.dataset.Dataset;
+
+import java.util.Collection;
+
 public class GspanAnalysis {
+
+    void run(Dataset dataset) {
+        Settings<String, String> settings = Settings.parse(new String[]{});
+        Collection<Graph<String,String>> graphs = null;
+        Collection<Fragment<String, String>> result = Miner.mine(graphs, settings);
+    }
+
 }
