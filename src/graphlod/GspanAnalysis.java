b4d972e380917d8ad80abed1562bfc66e6551b1e
Christian Dullweber <xchrdw@gmail.com>
2016-04-01 09:59:12 +0200

    finish gspan

--- src/graphlod/GspanAnalysis.java
+++ src/graphlod/GspanAnalysis.java
@@ -1,11 +1,17 @@
 package graphlod;
 
+import static de.parsemis.miner.environment.Debug.err;
+
 import de.parsemis.Miner;
 import de.parsemis.graph.Edge;
 import de.parsemis.graph.Graph;
 import de.parsemis.graph.HPMutableGraph;
 import de.parsemis.miner.environment.Settings;
 import de.parsemis.miner.general.Fragment;
+import de.parsemis.parsers.GraphParser;
+import de.parsemis.parsers.IntLabelParser;
+import de.parsemis.parsers.SimpleDirectedGraphParser;
+import de.parsemis.parsers.StringLabelParser;
 import de.parsemis.parsers.antlr.DotParser;
 import graphlod.dataset.Dataset;
 
@@ -19,10 +25,17 @@ import org.jgraph.graph.DefaultEdge;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Maps;
 
+
+
 public class GspanAnalysis {
 
     void run(Dataset dataset) {
-        Settings<String, String> settings = Settings.parse(new String[]{});
+//        Settings.printUsage(err);
+        GraphParser<String, String> parser = new SimpleDirectedGraphParser(new StringLabelParser(), new StringLabelParser());
+        Settings<String, String> settings = Settings.parse(new String[]{"--graphFile=null", "--minimumFrequency=1"}, parser); // --threads=int
+        if (settings == null) {
+            Settings.printUsage(err);
+        }
         HPMutableGraph<String, String> graph = (HPMutableGraph<String, String>) settings.factory.newGraph(dataset.getName()).toHPGraph();
 
         final Map<String, Integer> nodes = Maps.newHashMap();
@@ -38,8 +51,6 @@ public class GspanAnalysis {
         for (final DefaultEdge e : edges) {
             graph.addEdgeIndex(nodes.get(e.getSource().toString()), nodes.get(e.getTarget().toString()), "edgeType", Edge.OUTGOING);
         }
-
-
 
         Collection<Graph<String,String>> graphs = ImmutableList.of(graph.toGraph());
         Collection<Fragment<String, String>> result = Miner.mine(graphs, settings);
