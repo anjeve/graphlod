70d48165627fb27c4ddccd29b63447caf3906e50
Christian Dullweber <xchrdw@gmail.com>
2016-04-01 12:48:42 +0200

    add output

--- src/graphlod/GspanAnalysis.java
+++ src/graphlod/GspanAnalysis.java
@@ -17,11 +17,14 @@ import graphlod.dataset.Dataset;
 
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
 
 import org.jgraph.graph.DefaultEdge;
 
+import com.google.common.collect.BiMap;
+import com.google.common.collect.HashBiMap;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Maps;
 
@@ -30,33 +33,53 @@ import com.google.common.collect.Maps;
 public class GspanAnalysis {
 
     void run(Dataset dataset) {
-//        Settings.printUsage(err);
+        Settings.printUsage(err);
         GraphParser<String, String> parser = new SimpleDirectedGraphParser(new StringLabelParser(), new StringLabelParser());
-        Settings<String, String> settings = Settings.parse(new String[]{"--graphFile=null", "--minimumFrequency=20"}, parser); // --threads=int
+        Settings<String, String> settings = Settings.parse(new String[]{"--graphFile=null", "--minimumFrequency=1", "--maximumNodeCount=3", "--findPathsOnly=true"}, parser); // --threads=int
         if (settings == null) {
             Settings.printUsage(err);
         }
         HPMutableGraph<String, String> graph = (HPMutableGraph<String, String>) settings.factory.newGraph(dataset.getName()).toHPGraph();
 
-        final Map<String, Integer> nodes = Maps.newHashMap();
+        final BiMap<String, Integer> nodes = HashBiMap.create();
 
         // Add nodes to graph.
         Set<String> vertices = dataset.getGraph().vertexSet();
         for (String vertex : vertices) {
-            nodes.put(vertex, graph.addNodeIndex(dataset.getClassForSubject(vertex)));
+            String vertexClass = dataset.getClassForSubject(vertex);
+            int id = graph.addNodeIndex(vertexClass);
+            nodes.put(vertex, id);
+//            graph.setNodeLabel(id, vertexClass);
         }
 
         // Add edges to graph.
         Set<DefaultEdge> edges = dataset.getGraph().edgeSet();
         for (final DefaultEdge e : edges) {
-            graph.addEdgeIndex(nodes.get(e.getSource().toString()), nodes.get(e.getTarget().toString()), "edgeType", Edge.OUTGOING);
+            graph.addEdgeIndex(nodes.get(e.getSource().toString()), nodes.get(e.getTarget().toString()), e.getUserObject().toString(), Edge.OUTGOING);
         }
 
+
         Collection<Graph<String,String>> graphs = ImmutableList.of(graph.toGraph());
+
         Collection<Fragment<String, String>> result = Miner.mine(graphs, settings);
+
+        BiMap<Integer, String> invNodes = nodes.inverse();
+
+        String ns = "http://www4.wiwiss.fu-berlin.de/diseasome/resource/";
+
         System.out.println("done - " + result.size());
         for (Fragment<String, String> fragment : result) {
             System.out.println(fragment);
+            Iterator<Edge<String, String>> it = fragment.toGraph().edgeIterator();
+            while(it.hasNext()) {
+                Edge<String, String> e = it.next();
+                String a = Integer.toString(e.getNodeA().getIndex());
+                String b = Integer.toString(e.getNodeB().getIndex());
+                String c1 = e.getNodeA().getLabel().replace(ns, "");
+                String c2 = e.getNodeB().getLabel().replace(ns, "");
+                String p = e.getLabel().replace(ns, "");
+                System.out.println(a + " (" + c1 + ") " + p + " " + b + " (" + c2 + ")");
+            }
         }
         //TODO output
     }
